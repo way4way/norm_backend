cd2a442fe657c63d3eb3614a07f1755bc32e4e0c
normals <danilzerlov630@gmail.com>
2023-04-27 23:01:04 +0800

    second commit

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ use std::io::Write;
 use std::process::Command;
 
 fn main() {
-    let output = Command::new("normal_method")
+    let output = Command::new("irisqualifier-method")
         .output()
         .expect("Failed to execute command");
 
     let mut out = std::io::stdout();
     out.write_all(output.stdout.as_slice())
-        .expect("Failed to expecute command");
+        .expect("Failed to output result");
 }
